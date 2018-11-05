f0c4df5070cb6b7c08ab37da2fd5b33d876ae34f
davide <davide@j8>
2018-11-05 15:42:17 +0100

    web j2ee app with maven

---
+++ src/main/resources/bz/idm/web/list.sql
@@ -0,0 +1,37 @@
+with repos as (
+select   xmlelement(name Repository, xmlattributes( id as id, 
+													name as name, 
+													timestamp as timestamp,
+													(
+													   with test as (
+															select unnest(xpath('/*/*',result_xml)) as x
+															  from log
+															 where repo_id = repositories.id
+															)
+															select count(*)
+															  from test
+													) as tot,
+													(
+													   with test as (
+															select unnest(xpath('/*/*',result_xml)) as x
+															  from log
+															 where repo_id = repositories.id
+															)
+															select coalesce(sum(case when (xpath('@status',x))[1]::varchar = 'OK' then 1 else 0 end),0)
+															  from test
+													) as ok),
+			       (
+			          select xmlelement(name root, xmlagg(result_xml))
+			            from log
+			           where repo_id = repositories.id
+			       )) as x
+  from repositories
+ where (name, timestamp) in (
+   select name, max(timestamp)
+    from repositories
+   group by 1
+   order by name
+ )
+)
+select xmlelement(name root, xmlagg(x))::varchar
+  from repos
